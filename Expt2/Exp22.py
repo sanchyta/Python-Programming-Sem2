9a0f7d0e021002278b7384b5d0f009393fbad826
Sanchita Jadhav <147413550+sanchyta@users.noreply.github.com>
2026-02-10 13:43:58 +0530

    Create Exp22.py

---
+++ Expt2/Exp22.py
@@ -0,0 +1,13 @@
+# Program to find a factorial using loops
+"""
+Created on Tue Feb 10 12:56:32 2026
+
+@author:Sanchita Jadhav
+"""
+
+n=int(input("Enter number:"))
+fact=1
+for i in range(1,n+1):
+    
+    fact=fact*i
+("Factorial:",fact)
