2dd4fac299031d0c9f065644f4d323952f64f0a2
Sanchita Jadhav <147413550+sanchyta@users.noreply.github.com>
2026-03-10 13:39:02 +0530

    Add files via upload

---
+++ Exp4/Exp4A-1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 07:44:25 2026
+
+@author: Sanchita Jadhav
+"""
+marks = [78, 85, 90, 67, 88]
+
+average = sum(marks) / len(marks)
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)
