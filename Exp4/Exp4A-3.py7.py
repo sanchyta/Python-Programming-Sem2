dae2e25d7e9e8d1ba58f0d86ceb62e6d4836bd5e
Sanchita Jadhav <147413550+sanchyta@users.noreply.github.com>
2026-03-10 13:40:51 +0530

    Add files via upload

---
+++ Exp4/Exp4A-3.py7.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 07:55:28 2026
+
+@author: Sanchita Jadhav
+"""
+feedback = "Great service and fast delivery"
+reversed_feedback = feedback[::-1]
+print("Reversed Feedback:", reversed_feedback)
