30f95236707b35d634d11de3b2014719b1570a43
Sanchita Jadhav <147413550+sanchyta@users.noreply.github.com>
2026-03-10 13:41:11 +0530

    Add files via upload

---
+++ Exp4/Exp4A-4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 08:02:33 2026
+
+@author:Sanchita Jadhav
+"""
+review = "good product good quality good price"
+word = "good"
+count = review.count(word)
+print("Word appears:", count, "times")
+
