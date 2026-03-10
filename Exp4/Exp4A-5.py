6d4effb8170f1fe38de2831ebc4dc0db31fcb760
Sanchita Jadhav <147413550+sanchyta@users.noreply.github.com>
2026-03-10 13:41:34 +0530

    Add files via upload

---
+++ Exp4/Exp4A-5.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 08:03:52 2026
+
+@author:Sanchita Jadhav
+"""
+numbers = [9876543210, 9123456780, 9876543210, 9988776655, 9123456780]
+unique_numbers = list(set(numbers))
+print("Unique Numbers:", unique_numbers)
