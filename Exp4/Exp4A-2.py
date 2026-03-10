12b7884681c75f46f01e4cce50da1010ff1e82a9
Sanchita Jadhav <147413550+sanchyta@users.noreply.github.com>
2026-03-10 13:39:37 +0530

    Add files via upload

---
+++ Exp4/Exp4A-2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 07:53:43 2026
+
+@author: Sanchita Jadhav
+"""
+prices = [250, 120, 75, 300, 90]
+total_bill = sum(prices)
+print("Total Bill:", total_bill)
+
