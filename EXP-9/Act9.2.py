2c696581fef94abf101f1042ac096dcb0397dac9
Sanchita Jadhav <147413550+sanchyta@users.noreply.github.com>
2026-05-03 18:43:35 +0530

    Create Act9.2.py

---
+++ EXP-9/Act9.2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 07:22:56 2026
+
+@author: SANCHITA JADHAV
+"""
+from datetime import date
+
+today = date.today()
+
+print("Today's Date:", today)
+print("Appointment booked successfully!")
